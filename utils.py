24246368200e19b7cebea83cbca9ebb4d6c2750e
CAMM961001 <miguel.castama@gmail.com>
2023-01-02 23:56:13 -0600

    Overall settings class

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 import requests
 
+class Settings:
+    def __init__(self):
+        self.page_title = "Miguel Castaneda"
+        self.layout = "wide"
+
+
 def load_lottie_url(url):
     r = requests.get(url)
     if r.status_code != 200:
