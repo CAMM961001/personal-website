1a32e5c9f02dec094cdeda678c32e37b751fc77e
CAMM961001 <miguel.castama@gmail.com>
2023-01-04 13:33:26 -0600

    Add external sites as project variables

--- utils.py
+++ utils.py
@@ -18,3 +18,13 @@ class Settings:
         self.pages = ['Home', 'Resume', 'Portfolio', 'Contact Me']
         self.menu_icons = ['house', 'book', 'folder2-open', 'envelope']
         self.fontsize = 20
+
+        #URL for other websites
+        self.itam_url = '<a href="https://mcdatos.itam.mx/es" style="color: #F63366; text-decoration:none;">ITAM</a>'
+        self.inai_url = '<a href="https://home.inai.org.mx/" style="color: #F63366; text-decoration:none;">INAI</a>'
+        self.cdas_url = '<a href="https://www.facebook.com/dataalgosocitam/" style="color: #F63366; text-decoration:none;">CDAS</a>'
+        self.pross_url = '<a href="https://ppross.mx/" style="color: #F63366; text-decoration:none;">PROSS</a>'
+        self.bilstein_url = '<a href="https://bilstein.com/en-us/about-us/" style="color: #F63366; text-decoration:none;">Bilstein</a>'
+        self.fsae_url = '<a href="https://www.fsaeonline.com/" style="color: #F63366; text-decoration:none;">FSAE</a>'
+        self.unam_url = '<a href="http://www.fi-a.unam.mx/" style="color: #F63366; text-decoration:none;">UNAM School of Engineering</a>'
+        self.unammotorsports = '<a href="https://unam.pro/" style="color: #F63366; text-decoration:none;">UNAM Motorsports</a>'
