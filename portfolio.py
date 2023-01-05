8bd82e0a562487173cc3a27aaf9d6c6d58662de3
CAMM961001 <miguel.castama@gmail.com>
2023-01-05 14:54:30 -0600

    First row of projects in portfolio class

--- portfolio.py
+++ portfolio.py
@@ -8,3 +8,20 @@ class Portfolio:
     def __init__(self):
         self.page_title = 'Portfolio'
         self.contets = './portfolio/projects.json'
+        with open(self.contets) as f:
+            self.data = json.load(f)['projects']
+        f.close()
+
+
+    def first_row(self):
+        with st.container():
+            col1, col2, col3 = st.columns(3, gap='large')
+            with col1:
+                st.caption(self.data[0]['contributors'])
+                description = ""
+                for line in self.data[0]['description']:
+                    description += line
+                
+                description = f'''<p style="text-align: justify; font-size: 18px">{description}</p>'''
+                st.markdown(description, unsafe_allow_html=True)
+                utils.render_button(url='https://camm961001.quarto.pub/un-enfoque-bayesiano/')
