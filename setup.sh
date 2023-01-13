cca83d8b9d01321e5e87ff30fc7b659354e7feba
CAMM961001 <miguel.castama@gmail.com>
2023-01-13 12:28:34 -0600

    add color theme to setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,11 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+echo "\
+[theme]\n\
+primaryColor = '#F63366'\n\
+backgroundColor = '#FFFFFF'\n\
+secondaryBackgroundColor = '#F0F2F6'\n\
+textColor = '#262730'\n\
+font = 'sans serif'" >> ~/.streamlit/config.toml
